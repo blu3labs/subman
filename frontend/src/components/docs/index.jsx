74d8cb61179cfe69dbdde7f4089f8c6b050a57fe
Mehmet Çığırgan <mehmet.cigirgan@ogr.deu.edu.tr>
2023-11-17 23:50:44 +0300

    front init

---
+++ frontend/src/components/docs/index.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./index.css";
+
+function Docs() {
+  return (
+    <div className="docsWrapper">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      voluptates eveniet tempora nostrum dicta. Nulla exercitationem neque minus
+      iusto voluptatum minima, nisi quae officia fuga quaerat sed omnis tempore
+      perferendis inventore modi facilis molestiae iste explicabo aliquam,
+      voluptate quasi! Eligendi excepturi voluptatibus soluta, sed nisi dolorum
+      delectus atque iste culpa quasi asperiores? Aliquam, quae maiores saepe
+      nulla numquam deserunt velit, illum accusantium dolorem, repellat autem.
+      Nemo repellat aperiam dolores, consequuntur minima esse aliquam impedit
+      non dignissimos reiciendis suscipit iure tempora nisi aliquid dolore,
+      <br/>
+      <br/>
+      accusamus rem porro hic quod architecto assumenda illum quas! Unde nemo
+      error impedit atque cupiditate repellendus sed! Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Ducimus id aspernatur ea repudiandae,
+      alias eum sed ab enim facilis unde quidem soluta magni recusandae maiores
+      debitis. Unde tempore temporibus expedita aperiam iusto mollitia officiis
+      obcaecati culpa, sed maiores iure exercitationem, optio sequi id maxime!
+      Non assumenda nesciunt eligendi pariatur labore quod dolorum dolore, nemo
+      perspiciatis architecto quibusdam sit, distinctio nulla consequatur iusto
+      sequi consequuntur facere! Nam facilis eaque, repellat error omnis
+      molestiae similique unde minus dicta. Assumenda voluptates quam eum
+      quibusdam qui inventore. Iusto saepe quam beatae voluptate hic natus
+      laborum, in ipsam magnam nihil accusamus iste, quae amet, aliquid
+      assumenda cupiditate ea voluptatem! Ut nisi iure voluptates suscipit, unde
+      vel. Excepturi officia maiores vel esse fugit, accusantium eaque nam
+      voluptate, temporibus minus nesciunt! Doloribus quia obcaecati eos
+      assumenda aspernatur animi, nam porro! Mollitia facere fugiat alias,
+      repudiandae ex necessitatibus inventore dolorum sunt minus et laboriosam,
+      ullam, aspernatur modi vero?
+      <br/>
+      <br/>
+      accusamus rem porro hic quod architecto assumenda illum quas! Unde nemo
+      error impedit atque cupiditate repellendus sed! Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Ducimus id aspernatur ea repudiandae,
+      alias eum sed ab enim facilis unde quidem soluta magni recusandae maiores
+      debitis. Unde tempore temporibus expedita aperiam iusto mollitia officiis
+      obcaecati culpa, sed maiores iure exercitationem, optio sequi id maxime!
+      Non assumenda nesciunt eligendi pariatur labore quod dolorum dolore, nemo
+      perspiciatis architecto quibusdam sit, distinctio nulla consequatur iusto
+      sequi consequuntur facere! Nam facilis eaque, repellat error omnis
+      molestiae similique unde minus dicta. Assumenda voluptates quam eum
+      quibusdam qui inventore. Iusto saepe quam beatae voluptate hic natus
+      laborum, in ipsam magnam nihil accusamus iste, quae amet, aliquid
+      assumenda cupiditate ea voluptatem! Ut nisi iure voluptates suscipit, unde
+      vel. Excepturi officia maiores vel esse fugit, accusantium eaque nam
+      voluptate, temporibus minus nesciunt! Doloribus quia obcaecati eos
+      assumenda aspernatur animi, nam porro! Mollitia facere fugiat alias,
+      repudiandae ex necessitatibus inventore dolorum sunt minus et laboriosam,
+      ullam, aspernatur modi vero?
+      <br/>
+      <br/>
+      accusamus rem porro hic quod architecto assumenda illum quas! Unde nemo
+      error impedit atque cupiditate repellendus sed! Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Ducimus id aspernatur ea repudiandae,
+      alias eum sed ab enim facilis unde quidem soluta magni recusandae maiores
+      debitis. Unde tempore temporibus expedita aperiam iusto mollitia officiis
+      obcaecati culpa, sed maiores iure exercitationem, optio sequi id maxime!
+      Non assumenda nesciunt eligendi pariatur labore quod dolorum dolore, nemo
+      perspiciatis architecto quibusdam sit, distinctio nulla consequatur iusto
+      sequi consequuntur facere! Nam facilis eaque, repellat error omnis
+      molestiae similique unde minus dicta. Assumenda voluptates quam eum
+      quibusdam qui inventore. Iusto saepe quam beatae voluptate hic natus
+      laborum, in ipsam magnam nihil accusamus iste, quae amet, aliquid
+      assumenda cupiditate ea voluptatem! Ut nisi iure voluptates suscipit, unde
+      vel. Excepturi officia maiores vel esse fugit, accusantium eaque nam
+      voluptate, temporibus minus nesciunt! Doloribus quia obcaecati eos
+      assumenda aspernatur animi, nam porro! Mollitia facere fugiat alias,
+      repudiandae ex necessitatibus inventore dolorum sunt minus et laboriosam,
+      ullam, aspernatur modi vero?
+    </div>
+  );
+}
+
+export default Docs;
