260ff78ca26646a554648f945961610e850c0bdc
Mehmet Çığırgan <mehmet.cigirgan@ogr.deu.edu.tr>
2023-11-18 14:31:02 +0300

    plugin fix 2

--- plugin/subman.js
+++ plugin/subman.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import "./subman.css";
 
 const Subman = () => {
   const [showPrice, setShowPrice] = useState(false);
 
   return (
-    <>
-      <button onClick={() => setShowPrice(!showPrice)}>
+    <div className="subman-wrapper">
+      <button onClick={() => setShowPrice(!showPrice)} className="subman-button">
         Subscription {showPrice && "0.1 USDT"}
       </button>
-    </>
+    </div>
   );
 };
 
